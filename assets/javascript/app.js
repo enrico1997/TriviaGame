489a2ffa48e0865bb724363b453b2135d905bdfb
Enrico Bernardo <ebernardo@gmail.com>
2017-08-30 18:48:36 -0700

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,193 @@
+//  This code will run as soon as the page loads.
+window.onload = function() {
+
+  //  Click events are done for us:
+  // $("#stop").click(stopwatch.stop);
+  // $("#reset").click(stopwatch.reset);
+  // $("#start").click(stopwatch.start);
+  $("#countdownTimer").html("00:12");
+
+};
+
+//Global Variables
+//********************************
+//  Variable that will hold our setInterval that runs the stopwatch
+var intervalId;
+//prevents the clock from being sped up unnecessarily
+var clockRunning = false;
+
+var correctCount = 0;
+var wrongCount = 0;
+var unansweredCount = 0;
+
+var index = 0;
+
+//Questions Object
+//********************************
+var questionArray = [
+  {
+
+    Question : "What is the fruit?",
+    Answers : [
+      { isCorrect: true, text: "pineapple"},
+      { isCorrect: false, text: "orange"},
+      { isCorrect: false, text: "jackfruit"},
+      { isCorrect: false, text: "kiwi"},
+    ],
+    correctImg : "assets/images/harry.jpg"
+  }, 
+  {
+
+    Question : "What is the animal?",
+    Answers : [
+      { isCorrect: true, text: "robin"},
+      { isCorrect: false, text: "elephant"},
+      { isCorrect: false, text: "lion"},
+      { isCorrect: false, text: "tiger"},
+    ],
+    correctImg : "assets/images/harry.jpg"
+  }, 
+  {
+
+    Question : "What is the color?",
+    
+    Answers : [
+      { isCorrect: true, text: "blue"},
+      { isCorrect: false, text: "green"},
+      { isCorrect: false, text: "red"},
+      { isCorrect: false, text: "yellow"}
+    ],
+    correctImg : "harry.jpg"
+  }
+];
+
+
+
+function outputQuestion(question) {
+  stopwatch.reset();
+  $(".question").html("<h3>" + question.Question + "</h3>");
+
+  for (var i = 0; i < question.Answers.length; i++) {
+    $("#button" + i).text(question.Answers[i].text).attr("value", question.Answers[i].isCorrect).show();
+  }
+}
+
+function getAnswer() {
+  $('.answerchoice').on('click', function() {
+    console.log('alert', index);
+    index++;
+    console.log ('click', index);
+    outputQuestion(questionArray[index]);
+  })
+
+  // Do "if" for answerCorrect here
+  // If index is greater than questionArray length, call showScore();
+}
+
+function startGame() {
+  //Reset Variables
+
+  $('.question').append('<button class="btn btn-primary btn-lg" id="startButton">Start</button>');
+  
+  $('#startButton').on('click', function() {
+    $(this).hide();
+    // stopwatch.start();
+    outputQuestion(questionArray[index]);
+    console.log(questionArray[index]);
+
+  getAnswer();
+
+  });
+
+
+}
+
+function answerCorrect() {
+  correctCount++;
+  alert("Correct!");
+  console.log("correct");
+}
+
+function answerWrong() {
+  wrongCount++;
+  alert("Incorrect!");
+  console.log("wrong");
+}
+
+function answerUnanswered() {
+  unansweredCount++;
+  alert("Time's Up!");
+  console.log("wrong due to time's up");
+}
+
+function showScore() {
+  $('.question').empty();
+  $('.question').append("<h2><p>" + correctCount + " correct</p></h2>");
+  $('.question').append("<h2><p>" + wrongCount + " wrong</p></h2>");
+  $('.question').append("<h2><p>" + unansweredCount + " unanswered</p></h2>");
+  countdownTimer.stop();
+  $('.timer').empty();
+}
+
+
+//  Stopwatch Object
+//********************************
+var stopwatch = {
+  time: 12, //time limit
+  lap: 1,
+
+  reset: function() {
+    stopwatch.time = 12;
+    $("#countdownTimer").html("00:12");
+  },
+
+  start: function() {
+      if (!clockRunning) {
+       intervalId = setInterval(stopwatch.count, 1000);
+        clockRunning = true;
+      }
+  },
+
+  stop: function() {
+    clearInterval(intervalId);
+    clockRunning = false;
+  },
+
+  count: function() {
+    //  Decrement time by 1 second, remember we cant use "this" here.
+    stopwatch.time--;
+    //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
+    //        and save the result in a variable.
+    var currentTime = stopwatch.timeConverter(stopwatch.time);
+    //  TODO: Use the variable you just created to show the converted time in the "display" div.
+    $('#countdownTimer').html(currentTime);
+  },
+
+  //  THIS FUNCTION IS DONE FOR US!
+  //  We do not need to touch it.
+
+  timeConverter: function(t) {
+
+    //  Takes the current time in seconds and convert it to minutes and seconds (mm:ss).
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+      minutes = "00";
+    }
+
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+  }
+};
+
+// Main Process
+// *****************************************
+startGame();
